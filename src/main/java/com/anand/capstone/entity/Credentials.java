54bb30f980e2b91c846e06a932f1d906192f0bcc
=Anand <=>
2024-05-03 22:29:08 +0530

    changed ID

--- src/main/java/com/anand/capstone/entity/Credentials.java
+++ src/main/java/com/anand/capstone/entity/Credentials.java
@@ -1,13 +1,14 @@
 package com.anand.capstone.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "user_credentials")
 public class Credentials {
-    @Column(unique = true)
+
+    @Id
     private String email;
 
     private String password;
