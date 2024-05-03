46ac3be28049447a26c87cb9ccceff201b679ab2
deepakkumark852 <deepakkumark52@gmail.com>
2024-05-03 22:19:51 +0530

    login

--- src/main/java/com/anand/capstone/repository/CredentialsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anand.capstone.repository;
-
-import com.anand.capstone.entity.Credentials;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CredentialsRepository extends JpaRepository<Credentials, Long> {
-
-}
